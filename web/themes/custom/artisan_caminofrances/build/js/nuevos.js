eb6312c75f3293a404b6cdb1d6515cbff202f8a7
Marcel Pernia <marcel.pernia@dosespacios.com>
2025-11-17 15:19:44 +0100

    Refactoriza el componente de carrusel de destacados en el tema artisan_caminofrances. Se eliminan estilos y scripts obsoletos, se implementa un nuevo archivo JavaScript para la funcionalidad del carrusel, y se añaden estilos CSS específicos para mejorar la presentación y responsividad del componente. Además, se actualiza la plantilla para incluir el nuevo carrusel.

--- web/themes/custom/artisan_caminofrances/build/js/nuevos.js
+++ web/themes/custom/artisan_caminofrances/build/js/nuevos.js
@@ -134,39 +134,3 @@
       }
     };
   })(jQuery, Drupal);
-
-  (function ($, Drupal) {
-    Drupal.behaviors.carruselDestacados = {
-      attach: function (context, settings) {
-        once('slickInit', '.carrusel-destacados', context).forEach(function(element) {
-          $(element).slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            dots: false,
-            arrows: true,
-            prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button"><span class="sr-only">Anterior</span></button>',
-            nextArrow: '<button class="slick-next" aria-label="Siguiente" type="button"><span class="sr-only">Siguiente</span></button>',
-            autoplay: false,
-            centerMode: false,
-            infinite: true,
-            responsive: [
-              {
-                breakpoint: 992,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1
-                }
-              },
-              {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
-            ]
-          });
-        });
-      }
-    };
-  })(jQuery, Drupal);
